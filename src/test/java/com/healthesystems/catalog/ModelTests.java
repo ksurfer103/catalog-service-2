631f804b841f757631d2bd6524ca8446ddaba0d0
Andrew Purdon <apurdon@healthesystems.com>
2016-10-05 08:40:35 -0400

    Changed model and services Aggregate is called catalog object is called product - refactored productPrice and tests

--- src/test/java/com/healthesystems/catalog/ModelTests.java
+++ src/test/java/com/healthesystems/catalog/ModelTests.java
@@ -1,6 +1,8 @@
 package com.healthesystems.catalog;
 
+import com.healthesystems.catalog.model.PriceLocale;
 import com.healthesystems.catalog.model.Product;
+import com.healthesystems.catalog.model.ProductPrice;
 import com.healthesystems.catalog.repository.CatalogRepository;
 
 import org.junit.Ignore;
@@ -14,13 +16,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by apurdon on 9/27/16.
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @ActiveProfiles("local")
-@Ignore
+//@Ignore
 public class ModelTests {
 
     @Autowired
@@ -46,10 +54,20 @@ public class ModelTests {
        // assertThat(product.getHcpc()).isEqualTo("9876");
     }
 
+    @Test
+    public void testCustomerPricing() {
 
+        // Product price
+        Set<ProductPrice> prices = new HashSet<ProductPrice>() ;
+        prices.add(new ProductPrice(null, BigDecimal.valueOf(10.00),new Date(), PriceLocale.XX,"***********","LIBERTY"));
+        Product p = new Product("1234999","9876","bandaid",prices);
 
+        catalogRepository.save(p);
 
+        // get product
+        Product savedProduct = catalogRepository.findBySku("1234999");
+        assertThat(savedProduct).isEqualTo(p);
 
-
+    }
 
 }
